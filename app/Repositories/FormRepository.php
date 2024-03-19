7c0722618417ca966d5ec8213911b2828307a8c4
tipomrsk <mateusbougleuxmaia@hotmail.com>
2024-03-19 19:09:07 -0300

    refact!:

--- app/Repositories/FormRepository.php
+++ app/Repositories/FormRepository.php
@@ -62,14 +62,12 @@ class FormRepository implements FormRepositoryInterface
     public function listByUser(string $user_uuid)
     {
         try{
-
             return $this->model->select('forms.uuid', 'forms.name', 'forms.description', 'forms.style', 'forms.webhook_url')
                 ->join('users', 'forms.user_id', 'users.id')
                 ->where('users.uuid', $user_uuid)
                 ->get();
         }
         catch (Exception $e) {
-            dd($e->getMessage());
             Log::error($e->getMessage());
 
             throw new Exception('Form not found');
